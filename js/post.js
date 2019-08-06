1982e64ea3b492453c49190e75eaf66ae606d62e
Naman <namangovila@gmail.com>
2019-08-06 14:31:04 +0530

    Fixed comments stacking  Please enter the commit message for your changes. Lines starting

--- js/post.js
+++ js/post.js
@@ -1,31 +1,38 @@
 function edit(){
-	document.getElementById("btn").innerHTML = "Save <i class='fa fa-floppy-o'></i>"
+	document.getElementsByClassName("btns")[0].innerHTML = "Save <i class='fa fa-floppy-o'></i>"
 	document.getElementById("blogTitleNew").setAttribute("class","clicked");
 	document.getElementById("blogTitleNew").setAttribute("contentEditable","true");
 	document.getElementById("blogBody").setAttribute("class","clicked");
 	document.getElementById("blogBody").setAttribute("contentEditable","true");
-	document.getElementById("btn").setAttribute("onclick","save()");
+	document.getElementsByClassName("btns")[0].setAttribute("onclick","save()");
 
 }
 
 function save(){
 	document.getElementById("blogTitleNew").setAttribute("contentEditable","false");
 	document.getElementById("blogBody").setAttribute("contentEditable","false");
-	document.getElementById("btn").innerHTML = "Edit  <i class='fa fa-pencil-square-o'>"
+	document.getElementsByClassName("btns")[0].innerHTML = "Edit  <i class='fa fa-pencil-square-o'>"
 	var element = document.getElementById("blogTitleNew");
     element.className = element.className.replace(/\bclicked\b/g, "");
     var element1 = document.getElementById("blogBody");
     element1.className = element1.className.replace(/\bclicked\b/g, "");
-  	document.getElementById("btn").setAttribute("onclick","edit()");
+  	document.getElementsByClassName("btns")[0].setAttribute("onclick","edit()");
 }
 
-function addComments(comment){
-        var addEventName = comment.value +'<br>';
-       
-        var a = document.getElementById('userComments');
-        document.getElementById('commentsSaved').innerHTML += '<p class = "paraText">'+addEventName+'</p>';          
-        a.value=a.defaultValue;
-    }
+    function addComments(comment)
+{
+    if(comment.value == "")
+    	alert("Empty Comment, Please type in something first");
+    else{
+    var cr = document.createElement('p');
+	cr.innerHTML = comment.value;
+	cr.setAttribute("class", "paraText")
+	var m = document.getElementById('commentsSaved')/*.appendChild(cr);*/  
+	m.insertBefore(cr,m.childNodes[0]);
+	document.getElementById('userComments').value = "";
+  	
+}}
+
 var numberOfClicks=0;
 function postLiked(){
         document.getElementById("likeBlog").innerHTML = "<i class='fa fa-thumbs-up'></i> Liked!";
