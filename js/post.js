8790a23ca423828369cbe0123edea594aeda31f0
Naman <namangovila@gmail.com>
2019-08-06 17:50:52 +0530

    Covered corner case for post page

--- js/post.js
+++ js/post.js
@@ -19,31 +19,30 @@ function save(){
   	document.getElementsByClassName("btns")[0].setAttribute("onclick","edit()");
 }
 
-    function addComments(comment)
-{
-    if(comment.value == "")
+function addComments(comment){
+	if(comment.value == "")
     	alert("Empty Comment, Please type in something first");
     else{
-    var cr = document.createElement('p');
-	cr.innerHTML = comment.value;
-	cr.setAttribute("class", "paraText")
-	var m = document.getElementById('commentsSaved')/*.appendChild(cr);*/  
-	m.insertBefore(cr,m.childNodes[0]);
-	document.getElementById('userComments').value = "";
-  	
-}}
+	    var cr = document.createElement('p');
+		cr.innerHTML = comment.value;
+		cr.setAttribute("class", "paraText")
+		var m = document.getElementById('commentsSaved');
+		m.insertBefore(cr,m.childNodes[0]);
+		document.getElementById('userComments').value = "";
+	}
+}
 
 var numberOfClicks=0;
 function postLiked(){
-        document.getElementById("likeBlog").innerHTML = "<i class='fa fa-thumbs-up'></i> Liked!";
-        document.getElementById("likeBlog").setAttribute("class","btns")
-        numberOfClicks += 1;
-        if(numberOfClicks!=0){
-            if (numberOfClicks==1){
-                document.getElementById('commentCount').innerHTML = numberOfClicks + " person likes this!";
-            }
-            else {
-                document.getElementById('commentCount').innerHTML = numberOfClicks + " people have liked this!";
-            }
-            }
+	document.getElementById("likeBlog").innerHTML = "<i class='fa fa-thumbs-up'></i> Liked!";
+    document.getElementById("likeBlog").setAttribute("class","btns")
+    numberOfClicks += 1;
+    if(numberOfClicks!=0){
+        if (numberOfClicks==1){
+            document.getElementById('commentCount').innerHTML = numberOfClicks + " person likes this!";
         }
+        else{
+            document.getElementById('commentCount').innerHTML = numberOfClicks + " people have liked this!";
+        }
+    }
+}
