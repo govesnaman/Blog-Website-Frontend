252600feaa923bdc82de7d70af860bd4aec76f0b
Naman <namangovila@gmail.com>
2019-08-10 16:33:37 +0530

    fixed post number same in all post

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,6 +1,7 @@
+//The function below loads when the webpage is loaded and creates the posts.
 function code(){
 	var i;
-	for(i = 0; i<5; i++){
+	for(i = 1; i<=5; i++){
 		var a = document.getElementsByClassName("allPostsContainer")[0];
 		var b = document.createElement("div");
 		b.setAttribute("class","post");
@@ -20,7 +21,7 @@ function code(){
 		f.setAttribute("data-target","#popup");
 		var g = document.createElement("p");
 		g.setAttribute("class","post-title");
-		g.innerHTML = "Hello there ! This is Post 1. ";
+		g.innerHTML = "Hello there ! This is Post "+i+"." ;
 		var h = document.createElement("p");
 		h.setAttribute("class","post-desc");
 		h.innerHTML = "Jimi Hendrix was arguably the greatest instrumentalist in the history of rock music.[1] Hendrix expanded the range and vocabulary of the electric guitar into areas no musician had ever ventured before. His boundless drive, technical ability and creative application of such effects as distortion forever transformed the sound of rock and roll. While creating his unique musical voice and guitar style, Hendrix synthesized diverse genres, including blues, R&B, soul, British rock, American folk music, 1950s rock and roll, psychedelic and jazz.";
